71023ed38da6fab6f41bab9d26f7dc61d6987f98
kevin mitchell <kmitch137@gmail.com>
2023-07-27 18:49:49 -0700

    final touches

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
+const app = express();
+const port = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+
 
 const hbs = exphbs.create();
 
@@ -42,5 +42,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Example app listening at http://localhost:${port}...'));
+  app.listen(port, () => console.log(`'Example app listening at http://localhost:${port}...'`));
 });
